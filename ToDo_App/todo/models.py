8c51c2deb8eaeb6cad77484e87f831b772eeef3c
Vishal <vishaldharmini@gmail.com>
2025-07-07 03:14:13 +0530

    added tabular view and search functionality to admin page

--- ToDo_App/todo/models.py
+++ ToDo_App/todo/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-# Create your models here.
-Class Task(models.model):
-    task = models.charField(max_length = 100)
-    is_completed = models.BooleanField(default = False)
+class Task(models.Model):
+    task = models.CharField(max_length=250)
+    is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
