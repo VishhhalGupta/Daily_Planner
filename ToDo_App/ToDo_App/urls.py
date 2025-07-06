ae71d96d32a3d26f39605665cb23362352ef9071
Vishal <vishaldharmini@gmail.com>
2025-07-07 03:32:14 +0530

    staged till adding tasks

--- ToDo_App/ToDo_App/urls.py
+++ ToDo_App/ToDo_App/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home')
+    path('', views.home, name = 'home'),
+
+
+    # todo
+    path('todo/', include('todo.urls')),
 ]
